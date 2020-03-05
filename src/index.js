429dbd66db92a3739cda8739238767f8c96c045a
Vitali Kuzmich <v.kuzmich@mail.ru>
2020-03-05 12:12:02 +0300

    feat: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map((val) => val.join(''));
+  for(i = 0; i < brackets.length; i++) {
+  	if (str.includes(brackets[i])) {
+  		str = str.replace(brackets[i], '');
+  		i = -1;
+  	}
+  }
+  return !str;
 }
